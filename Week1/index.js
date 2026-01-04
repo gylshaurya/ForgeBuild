d36fcae493ee4ad35d545fccc864bc884875a713
Shaurya <shaurya.shauryagoyal123@gmail.com>
2026-01-05 02:53:51 +0530

    Add web3 section

---
+++ Week1/index.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('web3-form');
+const number = document.getElementById('input');
+const submitBtn = document.getElementById('submit-btn');
+
+form.addEventListener('submit', (e) =>{
+
+    e.preventDefault();
+
+
+});
