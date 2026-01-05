cc7f14c06922aff4f40fe1b495a57246247f3316
Shaurya <shaurya.shauryagoyal123@gmail.com>
2026-01-05 20:11:44 +0530

    Add Smart contract code and Add get button

---
+++ Week1/SimpleStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SimpleStorage{
+    uint256 number;
+
+    function store( uint256 num) public {
+        number = num; 
+    }
+
+    function get() public view returns(uint256) {
+        return number;   
+    }
+
+} 
--- Week1/index.js
+++ Week1/index.js
@@ -3,13 +3,14 @@ import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@6.8.1/dist/ethers.mi
 const form = document.getElementById('web3-form');
 const input = document.getElementById('input');
 const output = document.getElementById('output');
+const submitBtn = document.getElementById('submit-btn');
+const getBtn = document.getElementById('get-btn');
 
 const contractAdd = "0x21638bD3019BfbD4Fb7B3d833b1F3F3E07BC2d60";
 
 let signer;
 let provider;
 let contract;
-let network;
 
 const abi = [
   "function store(uint256 num)",
@@ -20,24 +21,28 @@ form.addEventListener('submit', async (e) =>{
     
     e.preventDefault();
     await storeValue(input.value);
-    await getValue();
+    submitBtn.innerText = `Upload Successful`
+});
 
+getBtn.addEventListener( 'click', async(e)=> {
+    await getValue();
+    getBtn.innerText = `Fetch Successful`
 });
 
 async function storeValue (num){
     output.innerText = "";
-    window.alert("Storing Number on Sepolia");
 
     provider = new ethers.BrowserProvider(window.ethereum);
     signer = await provider.getSigner();
     contract = new ethers.Contract(contractAdd, abi, signer);
 
     const tx = await contract.store(num);
+    submitBtn.innerText = `Uploading... `
     await tx.wait();
 }
 
 async function getValue() {
-    window.alert("Getting Number from Sepolia");
+    getBtn.innerText = `Fetching... `
     const storedVal = await contract.get();
     output.innerText = storedVal.toString();
 }
