cf2d9fd7ed2bb4dffa1ac322a88a8a5f61060028
Shaurya <shaurya.shauryagoyal123@gmail.com>
2026-01-05 17:50:30 +0530

    Add store and get data from Sepolia using Solidity Contract

--- Week1/index.js
+++ Week1/index.js
@@ -1,10 +1,43 @@
+import { ethers } from "https://cdn.jsdelivr.net/npm/ethers@6.8.1/dist/ethers.min.js";
+
 const form = document.getElementById('web3-form');
-const number = document.getElementById('input');
-const submitBtn = document.getElementById('submit-btn');
+const input = document.getElementById('input');
+const output = document.getElementById('output');
 
-form.addEventListener('submit', (e) =>{
+const contractAdd = "0x21638bD3019BfbD4Fb7B3d833b1F3F3E07BC2d60";
 
+let signer;
+let provider;
+let contract;
+let network;
+
+const abi = [
+  "function store(uint256 num)",
+  "function get() view returns (uint256)"
+]
+
+form.addEventListener('submit', async (e) =>{
+    
     e.preventDefault();
-
+    await storeValue(input.value);
+    await getValue();
 
 });
+
+async function storeValue (num){
+    output.innerText = "";
+    window.alert("Storing Number on Sepolia");
+
+    provider = new ethers.BrowserProvider(window.ethereum);
+    signer = await provider.getSigner();
+    contract = new ethers.Contract(contractAdd, abi, signer);
+
+    const tx = await contract.store(num);
+    await tx.wait();
+}
+
+async function getValue() {
+    window.alert("Getting Number from Sepolia");
+    const storedVal = await contract.get();
+    output.innerText = storedVal.toString();
+}
